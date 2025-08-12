5071dc54d774f230491dd7ca52a5aba661c1523c
manpreet singh <msingh.18.2001@gmail.com>
2025-08-12 23:40:32 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ const Footer: React.FC = () => {
               </div>
               <div className="flex items-center space-x-2">
                 <Phone className="h-4 w-4 text-yellow-500" />
-                <span className="text-gray-300">+91 9971264330</span>
+                <span className="text-gray-300">+91 9818084198</span>
               </div>
               <div className="flex items-center space-x-2">
                 <Mail className="h-4 w-4 text-yellow-500" />
